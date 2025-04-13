e86012a907be565bf4ac8870c9c29a839c84f688
Karime <karime.soriano.ortiz@gmail.com>
2025-04-13 17:27:30 +0200

    Zweite PUnkt

--- Versuch01Teil1/Variablen.cpp
+++ Versuch01Teil1/Variablen.cpp
@@ -16,8 +16,9 @@ int main()
 {
     int iErste = 0;
     int iZweite = 0;
-    int iSumme=0;
-    int iQuotient =0;
+    int iSumme, iQuotient;
+    double dSumme=0;
+    double dQuotient =0;
     
     // Hier folgt Ihr eigener Code
     cout<<"Give 1 integer number: "; cin>>iErste;
@@ -27,6 +28,14 @@ int main()
     
     cout<<"The sum is: "<<iSumme<<endl;
     cout<<"The quotient is: "<<iQuotient<<endl;
+
+    //calculate with double
+    dSumme = static_cast<double>(iErste)+iZweite;
+    dQuotient = static_cast<double>(iErste)/iZweite;
+    cout<<"Result: "<<endl;
+    cout<<"The result is: "<<iSumme<<endl;
+    cout<<"The Quotient is: "<<dQuotient<<endl;
+
     return 0;
     
 }
